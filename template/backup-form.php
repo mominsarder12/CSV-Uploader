a5877c8670d51753b63bd646b7a5082afd64b994
momin1212 <momin.sarder1212@gmail.com>
2025-05-06 18:05:39 +0600

    attached export csv option

---
+++ template/backup-form.php
@@ -0,0 +1,5 @@
+<h3>Backup Form</h3>
+
+<form action="" id="backup_form" method="post"  enctype="multipart/form-data">
+    <?php submit_button('Export Data','primary','ms_tdbc_submit'); ?>
+</form>
